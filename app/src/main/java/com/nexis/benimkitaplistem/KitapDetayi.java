cf1ee0fcaf81f4800ec3eb72b34c020340cc87b0
Nexis <mustafa.salih.eybek.07@gmail.com>
2022-10-29 16:35:41 +0300

    Initial commit

---
+++ app/src/main/java/com/nexis/benimkitaplistem/KitapDetayi.java
@@ -0,0 +1,31 @@
+package com.nexis.benimkitaplistem;
+
+import android.graphics.Bitmap;
+
+public class KitapDetayi {
+    private String kitapAdi, kitapYazari, kitapOzeti;
+    private Bitmap kitapResimi;
+
+    public KitapDetayi(String kitapAdi, String kitapYazari, String kitapOzeti, Bitmap kitapResimi) {
+        this.kitapAdi = kitapAdi;
+        this.kitapYazari = kitapYazari;
+        this.kitapOzeti = kitapOzeti;
+        this.kitapResimi = kitapResimi;
+    }
+
+    public String getKitapAdi() {
+        return kitapAdi;
+    }
+
+    public String getKitapYazari() {
+        return kitapYazari;
+    }
+
+    public String getKitapOzeti() {
+        return kitapOzeti;
+    }
+
+    public Bitmap getKitapResimi() {
+        return kitapResimi;
+    }
+}
